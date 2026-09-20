1a556887ebd817e3a63d9b32b082988b9561a7c2
rdhankhar <rahulqwert1212@gmail.com>
2026-09-20 15:11:42 +0530

    Add Relational Operators file

--- Prime 3.0  AI/ML/3-style.py
+++
@@ -1,7 +0,0 @@
-is_name = "Rahul" # Snake case
-isName = "Rahul" # Camel case
-IsName = "Rahul" # Pascal case          
-
-print(is_name)
-print(isName)
-print(IsName)
---
+++ Prime 3.0  AI/ML/Python - Data/5-relational.py
@@ -0,0 +1,9 @@
+# Relational Operators
+a = 10
+b = 20
+print("a is equal to b :" ,(a==b))
+print("a is not equal to b :" ,(a!=b))
+print("a is greater than b :" ,(a>b))
+print("a is less than b :" ,(a<b))
+print("a is greater than or equal to b :" ,(a>=b))
+print("a is less than or equal to b :" ,(a<=b))
