e84f72b9bf2fd1e7c7fd0bc097a3d3e48017fe9f
rdhankhar <rahulqwert1212@gmail.com>
2026-09-20 14:49:22 +0530

    Add Arithmetic operators  file

---
+++ Prime - 3.0/Python - Data/4-airthmetic.py
@@ -0,0 +1,10 @@
+# Arithmetic operations in python
+a = 10
+b = 20
+
+print("Addition of a and b is :" ,(a+b))
+print("Subtraction of a and b is :" ,(a-b))
+print("Multiplication of a and b is :" ,(a*b))
+print("Division of a and b is :" ,(a/b))
+print("Modulus of a and b is :" ,(a%b))
+print("Exponent of a and b is :" ,(a**b))
