af0653e5c7e1069d80d6040781957a7607a3c4af
rdhankhar <rahulqwert1212@gmail.com>
2026-09-20 15:24:58 +0530

    Add Assignment operators file

---
+++ Prime 3.0  AI/ML/Python - Data/6-assignment.py
@@ -0,0 +1,17 @@
+# Assignment Operators
+
+a = 10
+
+print("a is :" , a)
+a += 5
+print("a is :" , a)
+a -= 5
+print("a is :" , a)
+a *= 5
+print("a is :" , a)
+a /= 5
+print("a is :" , a)
+a %= 5
+print("a is :" , a)
+a **= 5
+print("a is :" , a)
