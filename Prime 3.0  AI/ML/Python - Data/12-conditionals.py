0d53b214edca0a8de99e528689419a5492f0d724
rdhankhar <rahulqwert1212@gmail.com>
2026-09-20 23:03:54 +0530

    Add conditionals statement file

---
+++ Prime 3.0  AI/ML/Python - Data/12-conditionals.py
@@ -0,0 +1,61 @@
+# if/else/elif
+
+# age = int(input("Enter age : "))
+
+# if age >= 18:
+#     print("You can drive")
+#     print("You can vote")
+# else:
+#     print("You can't drive")
+
+# colour = input("Enter colour : ")
+
+# if colour == "Red":
+#     print("Stop")
+# elif colour == "Green":
+#     print("Go")
+# elif colour == "Yellow":
+#     print("Look")
+# else:
+#     print("Not a valid colour")
+
+# Practice question
+
+# age = int(input("Enter age : "))
+
+# if age < 13:
+#     print("Child")
+# elif age >= 13 and age < 18:
+#     print("Teenager")
+# elif age >= 18 and age < 60:
+#     print("Adult")
+# elif age >= 60 and age <= 100:
+#     print("Old")
+# else: 
+#     print("Not a valid age")
+
+# username = input("Enter username : ")
+# password = int(input("Enter password : "))
+
+# if username == "Rahul" and password == 1150:
+#     print("Login Successful")
+# elif username != "Rahul":
+#     print("Wrong username")
+# else :
+#     print("Wrong password")
+
+# n = int(input("Enter value of n : "))
+
+# if n % 7 == 0:
+#     print("Number is divisible by 7")
+# else:
+#     print("Number is not divisible by 7")
+
+# odd even 
+
+n = int(input("Enter value of n : "))
+
+if n % 2 == 0:
+    print("Even number")
+else:
+    print("Odd number")
