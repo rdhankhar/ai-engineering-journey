947385c22ac56ca95cf098403b2e745f77eb0719
rdhankhar <rahulqwert1212@gmail.com>
2026-09-21 10:43:00 +0530

    Add for loop file

---
+++ Prime 3.0  AI/ML/Python - Data/18-for_loop.py
@@ -0,0 +1,23 @@
+# word = "Rahul"
+
+# for i in word:
+#     print(i)
+
+# for i in range(5):
+#     print(i+1)
+
+# word = "Artificial intelligence"
+
+# count = 0
+# for var in word:
+#     if (var == 'i'):
+#         count += 1
+        
+# print("Count is : ",count )
+
+string = "Rahul"
+
+if 'u' in string:
+    print("u exists in string")
+else:
+    print("Not exists")
