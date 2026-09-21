5de46486c35f935dfd9165990852cfc118fd6276
rdhankhar <rahulqwert1212@gmail.com>
2026-09-21 10:01:55 +0530

    Add table file

---
+++ Prime 3.0  AI/ML/Python - Data/16-table.py
@@ -0,0 +1,11 @@
+n = int(input("Enter value of n : "))
+
+# i = 1
+# while (i <= 10):
+#     print(n ,"X", i ,"=" , n*i )
+#     i += 1
+
+i = 0
+while (i < 10):
+    print(n ,"X", i + 1 ,"=" , n * (i+1) )
+    i += 1
