47b3122d13b2579e1a5ff82cf3b803ac9a11e884
rdhankhar <rahulqwert1212@gmail.com>
2026-09-21 09:51:14 +0530

    Add while loop file

---
+++ Prime 3.0  AI/ML/Python - Data/15-loops_while.py
@@ -0,0 +1,17 @@
+# count = 1
+# while (count <= 10):
+#     print("Hii Rahul" , count)
+#     count += 1
+# print("After loop , count is :", count)
+
+# practice question
+
+# i = 1
+# while (i <= 10):
+#     print(i)
+#     i += 1
+
+i = 10
+while (i >= 1):
+    print(i)
+    i -= 1
