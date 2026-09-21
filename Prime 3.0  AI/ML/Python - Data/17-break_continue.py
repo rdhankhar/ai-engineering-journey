2433af896e3668c978c5dca6101f02ad8d85c79e
rdhankhar <rahulqwert1212@gmail.com>
2026-09-21 10:24:47 +0530

    Add break and continue file

---
+++ Prime 3.0  AI/ML/Python - Data/17-break_continue.py
@@ -0,0 +1,40 @@
+# Break statement
+# i = 1
+
+# while (i <= 10):
+#     # if (i % 9 == 0) :
+#     if (i == 7) :
+#         break
+#     print(i)
+#     i += 1
+
+# continue statement
+
+# i = 1
+# while (i <= 10):
+#     if (i % 3 == 0) :
+#     # if (i == 7) :
+#         i += 1
+#         continue
+#     print(i)
+#     i += 1
+
+# i = 1
+
+# while (i <= 10):
+#     if (i % 2 == 0) :
+#     # if (i == 7) :
+#         i += 1
+#         continue
+#     print(i)
+#     i += 1
+
+i = 0
+while (i < 10):
+    i += 1
+    if (i % 2 == 0) :
+    # if (i == 7) :
+        continue
+    print(i)
+
+
