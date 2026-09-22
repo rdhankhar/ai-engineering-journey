63a4b77e1c09df167e4a08c93b1d3c1e6e01bee5
rdhankhar <rahulqwert1212@gmail.com>
2026-09-22 22:47:46 +0530

    Add function File

--- Prime 3.0  AI/ML/Python - Data/21-function.py
+++ Prime 3.0  AI/ML/Python - Data/21-function.py
@@ -1,2 +1,38 @@
-a = 20
-print(a)
+# def hello():
+#     print("Hello rahul")
+
+# hello()
+
+# Sum 
+
+# def sum(a , b):
+#     s = a + b
+#     return s
+#     # return a + b
+
+# # ans = sum(10,30)
+# # print("Sum of a and b is :",ans)
+# print("Sum of a and b is :",sum(20,48))
+
+# Average 
+
+# def average(a , b , c):
+#         avg = (a + b + c) / 3
+#         return avg
+# #     return (a + b + c) / 3
+
+# # avg = average(10,30,70)
+# # print("Sum of a and b is :",avg)
+# print("Average of three numbers is :",average(20,48,76))
+
+# Default function
+
+def sum(a , b = 13):
+    return a + b
+
+print("Sum is :",sum(20))
+
+# def sum(a = 10, b = 13):
+#     return a + b
+
+# print("Sum is :",sum())
