2338931d6732b26f7185408fdca8c6ab0df31c19
rdhankhar <rahulqwert1212@gmail.com>
2026-09-22 23:00:30 +0530

    Add lambda function file

---
+++ Prime 3.0  AI/ML/Python - Data/22-lambda_function.py
@@ -0,0 +1,14 @@
+# sum 
+
+# sum = lambda a , b ,c : a + b + c
+# print("Sum is :",sum(10,40,55))
+
+# multiply
+
+# mul = lambda a , b ,c : a * b * c
+# print("Product is :",mul(10,40,55))
+
+# Average
+
+avg = lambda a , b ,c : (a + b + c)/3
+print("Average is :",avg(10,40,55))
