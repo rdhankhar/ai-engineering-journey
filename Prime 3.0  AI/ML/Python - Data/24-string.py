7a7092ceb452fcb4dfffbff2301d776a08e3b950
rdhankhar <rahulqwert1212@gmail.com>
2026-09-22 23:53:39 +0530

    Add new String file

---
+++ Prime 3.0  AI/ML/Python - Data/24-string.py
@@ -0,0 +1,17 @@
+# word = "Rahul"
+# print(word)
+
+# Concatenation
+
+# word1 = "Rahul"
+# word2 = "Dhankhar"
+
+# print(word1 + " " + word2 )
+
+# word = "Rahul"
+# print(word[2])
+
+word = "Rahul"
+
+for ch in word:
+    print(ch)
