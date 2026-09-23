bcd75f7666929ab7957537fa9d12783477ab7931
rdhankhar <rahulqwert1212@gmail.com>
2026-09-23 23:24:23 +0530

    Add tuples method file

---
+++ Prime 3.0  AI/ML/Python - Data/31-tuples_method.py
@@ -0,0 +1,11 @@
+tup = (12,43,14,14,45,65,14,76,32,34)
+
+# sum = 0
+# for val in tup:
+#     sum += val
+    
+# print(f"Sum is : {sum}")
+# print(tup[2:])
+
+print(tup.index(32))
+print(tup.count(14))
