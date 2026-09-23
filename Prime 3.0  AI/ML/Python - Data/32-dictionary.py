8f9f081312e53e6934bf88c5947fdacb0f09e5a8
rdhankhar <rahulqwert1212@gmail.com>
2026-09-23 23:49:33 +0530

    Add dictionary file

---
+++ Prime 3.0  AI/ML/Python - Data/32-dictionary.py
@@ -0,0 +1,10 @@
+info = {
+    "Name" : "Rahul",
+    "Subjects" :["Maths","English","Python","Dsa","Java"] ,
+    "Marks" : [94,95,90,87,98],
+    "Cgpa" : 9.5
+}
+info["Cgpa"] = 9.6
+print(info)
+print(info["Marks"])
+print(type(info))
